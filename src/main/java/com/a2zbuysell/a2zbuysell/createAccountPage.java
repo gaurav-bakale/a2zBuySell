4c0934d70dff513c5ab4cf35916720639f964f69
Gaurav Bakale <bakalegaurav@gmail.com>
2024-12-02 15:01:53 -0500

    create account screen updated

---
+++ src/main/java/com/a2zbuysell/a2zbuysell/createAccountPage.java
@@ -0,0 +1,107 @@
+package com.a2zbuysell.a2zbuysell;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class createAccountPage {
+
+    public Button goBackButton;
+    @FXML
+    private VBox FullPageVbox;
+
+    @FXML
+    private Button aboutButton;
+
+    @FXML
+    private HBox appTitleBar;
+
+    @FXML
+    private Label appTitleLabel;
+
+    @FXML
+    private Button contactButton;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Text createAccountMessageText;
+
+    @FXML
+    private Label createNewAccount;
+
+    @FXML
+    private PasswordField email;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private PasswordField firstName;
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private PasswordField lastName;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private PasswordField phoneNumber;
+
+    @FXML
+    private Label phoneNumberLabel;
+
+    @FXML
+    private Button privacyButton;
+
+    @FXML
+    private Button termsButton;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private Label userNameLabel;
+
+    @FXML
+    void createAccountButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(MouseEvent event) {
+
+    }
+
+    public void aboutButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void contactButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void termsButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void privacyButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void goBackButtonClick(ActionEvent actionEvent) {
+    }
+}
